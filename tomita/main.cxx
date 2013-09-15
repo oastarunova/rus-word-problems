f2e2c3dcc32b5a097f5ca42db205b9ece3b9bc13
Kirill Maslinsky <kirill@altlinux.org>
2013-09-15 23:49:37 +0400

    tomita tuning

--- tomita/main.cxx
+++ tomita/main.cxx
@@ -5,13 +5,14 @@
 
 S -> WPNumber;
 WPNumber -> Number<fw> interp (WPNum.WPid) Punct EOSent;           
-NP -> Word<gnc-agr[1], gram="A,~APRO,~ANUM", kwtype=none>* Word<gnc-agr[1], rt, kwtype=none, gram="S,~ANUM,~APRO"> interp (Entity.Root) Noun<gram="gen", kwtype=none>*;
-NP -> Word<kwtype=none, gram="S,~gen,~APRO"> interp (Entity.Root);
+NNP -> Noun<rt, kwtype=none, gram="~ANUM,~APRO,~PR"> interp (Entity.Root) NP<gram="gen">*;
+//NNP -> Noun<rt, kwtype=none, gram="~ANUM,~APRO,~PR"> interp (Entity.Root) Word<gram="S,gen", kwtype=none>*;
+NP -> Word<gnc-agr[1], gram="A,~APRO,~ANUM", kwtype=none>* NNP<gnc-agr[1], rt>; 
+//NP -> Word<kwtype=none, gram="S,~gen,~APRO"> interp (Entity.Root);
 S -> NP interp (Entity.Name);
 Date -> AnyWord<kwtype="дата_авт">;
 S -> Date;
 Number -> AnyWord<kwtype="число_авт">;
-//Number -> Word<gram="ANUM">+;
 Metric -> Word<kwtype=metric>;
 Quantity -> Number interp (Metric.Value) Word<kwtype=none,gram="gen,~ANUM"> interp (Metric.Type="количество"; Metric.Unit; Entity.Name);
 Quantity -> Number<rt> interp (Metric.Value) Metric<gram="gen"> interp (Metric.Type; Metric.Unit::not_norm);
@@ -19,22 +20,4 @@ Quantity -> 'в' Word<kwtype="время"> interp (Metric.Type; Metric.Unit::not
 Quantity -> 'за' Word<kwset=["время","количество"]>;
 S -> Quantity;
 S -> Quantity Noun<gram="gen">+ interp (Entity.Name);
-/*
-CmpMeasure -> "на" Quantity;
-CmpMeasure -> "в" AnyWord<kwtype="число_авт"> "раз";
-Dim -> Word<kwtype="размерность"> Word<gram="gen">+;
-Comparative -> (CmpMeasure) NP Adj<gram="comp"> NP; 
-Comparative -> CmpMeasure Adj<gram="comp"> NP; 
-Comparative -> NP CmpMeasure Adj<gram="comp"> NP; 
-Comparative -> NP Adj<gram="comp"> CmpMeasure; 
-Comparative -> NP Adj<gram="comp"> NP CmpMeasure;
-Distance -> ('расстояние') 'от' NP<gram="gen"> 'до' NP<gram="gen">;
-Distance -> 'из' NP<gram="gen"> 'в' NP<gram="acc">;
-//VP -> NP Verb<gram="indic">;
-//S -> VP;
-Personage -> (Adj<gnc-agr[1]>) Noun<gnc-agr[1], gram="anim", rt>;
-S -> Comparative;
-S -> Dim;
-S -> Distance;
-//S -> Personage;
-*/
+
